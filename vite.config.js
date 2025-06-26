d2b9b4ba9cf780302e22a319f206bfa3507fe16d
niprobin <niprobin@gmail.com>
2025-06-26 11:24:13 +0200

    godamnit

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,52 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'Body Music Radio',
-        short_name: 'BodyMusic',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#1C1B22',
-        theme_color: '#1C1B22',
-        description: 'Body Music Radio - La radio pour tout le monde',
-        icons: [
-          {
-            src: '/browser_icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+        "name": "Body Music",
+  "short_name": "Body Music",
+  "id": "/",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#1C1B22",
+  "theme_color": "#1C1B22",
+  "description": "Body Music Radio - La radio pour tout le monde",
+  "orientation": "portrait",
+  "dir": "ltr",
+  "prefer_related_applications": false,
+  "launch_handler": {
+    "client_mode": "auto"
+  },
+  "icons": [
+    {
+      "src": "/browser_icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "screenshots": [
+    {
+      "src": "/screenshot1.png",
+      "sizes": "538x1145",
+      "type": "image/png",
+      "label": "Home screen"
+    },
+    {
+      "src": "/screenshot2.png",
+      "sizes": "538x1145",
+      "type": "image/png",
+      "label": "Schedule"
+    },
+    {
+      "src": "/screenshot3.png",
+      "sizes": "538x1145",
+      "type": "image/png",
+      "label": "Last songs played"
+    }
+  ],
+  "categories": [
+    "music",
+    "audio",
+    "entertainment"
+  ]
       }
     })
   ]
